f55de2784c8a3278e6e96971ed6dcb96f13d2771
SubParSuperCar <elliot_grimm_2008@yahoo.com>
2025-12-22 23:16:32 -0800

    Remove and update project files.

--- SubPar Suite/App.xaml.cs
+++ SubPar Suite/App.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using ControlzEx.Theming;
 using System.Windows;
-using System.Windows.Threading;
 
 namespace SubPar_Suite;
 
@@ -10,22 +9,7 @@ public partial class App : Application
 	{
 		base.OnStartup(e);
 
-		AppDomain.CurrentDomain.UnhandledException += (sender, e) =>
-		{
-			if (e.ExceptionObject is Exception ex)
-				Shared.ShowError(ex, "Unhandled Background Exception",
-					"An unhandled exception was thrown in a background thread. The app will shut down after this dialog is closed.");
-		};
-
 		ThemeManager.Current.ThemeSyncMode = ThemeSyncMode.SyncAll;
 		ThemeManager.Current.SyncTheme();
-	}
-
-	private void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
-	{
-		Shared.ShowError(e.Exception, "Unhandled Exception",
-			"An unhandled exception was thrown in the UI thread. The app will shut down after this dialog is closed.");
-
-		e.Handled = true;
 	}
 }
